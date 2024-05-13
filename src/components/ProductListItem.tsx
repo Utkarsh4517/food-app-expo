64ea4ddf12c9b6a442baeb03e6a338fce42b9d35
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-05-13 23:31:18 +0530

    made productlistitem pressable

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,47 +1,52 @@
-import { View, Image, StyleSheet, Text } from "react-native";
+import { View, Image, StyleSheet, Text, Pressable } from "react-native";
 import { Product } from "../types";
 import products from "@/assets/data/products";
 import { defaultPizzaImage } from "../constants/default";
+import { Link } from "expo-router";
 
 type ProductListItemProps = {
-    product: Product;
-}
-const ProdctListItem = ({product}: ProductListItemProps) => {
-    return (
-      <View style={styles.container}>
-        <Image source={{uri: product.image || defaultPizzaImage}} style={styles.image}/>
-       <Text style={styles.title}>{product.name}</Text>
-       <Text style={styles.price}>${product.price}</Text>
-      </View>
-    );
-  
-  }
+  product: Product;
+};
+const ProdctListItem = ({ product }: ProductListItemProps) => {
+  return (
+    <Link href={"/product"} asChild>
+    <Pressable style={styles.container}>
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+        resizeMode="contain"
+      />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </Pressable>
+    </Link>
 
-  export default ProdctListItem;
+  );
+};
+
+export default ProdctListItem;
 
 const styles = StyleSheet.create({
-    container: {
-      backgroundColor: 'white',
-      padding: 10,
-      borderRadius: 20,
-      flex: 1,
-      maxWidth: '50%',
-    },
-    title: {
-      fontSize: 18,
-      fontWeight: '600',
-      marginVertical: 10,
-      color: 'black',
-    },
-  
-    price : {
-      color: 'darkgreen',
-      fontWeight: 'bold',
-    },
-    image:{
-      width: '100%',
-      aspectRatio: 1,
-    }
-    
-  });
-  
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginVertical: 10,
+    color: "black",
+  },
+
+  price: {
+    color: "darkgreen",
+    fontWeight: "bold",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+});
