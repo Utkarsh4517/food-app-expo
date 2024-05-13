9d33e5903e3f8de0fb460a0a2cffc3d06e2c19fc
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-05-13 23:14:31 +0530

    added flatlist content styling

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,16 @@
-import { StyleSheet, Image, FlatList } from 'react-native';
+import { StyleSheet, Image, FlatList } from "react-native";
 
-import products from '@/assets/data/products';
-import ProdctListItem from '@/src/components/ProductListItem';
-
-
-
+import products from "@/assets/data/products";
+import ProdctListItem from "@/src/components/ProductListItem";
 
 export default function TabOneScreen() {
   return (
-    <FlatList data={products} renderItem={({item}) => <ProdctListItem product={item}/>  } numColumns={2}/>
+    <FlatList
+      data={products}
+      renderItem={({ item }) => <ProdctListItem product={item} />}
+      numColumns={2}
+      contentContainerStyle={{gap: 10, padding: 10}}
+      columnWrapperStyle={{gap: 10}}
+    />
   );
 }
