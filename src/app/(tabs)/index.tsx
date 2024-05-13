48b2dc703d7c1dffbbd92ca24b333da3c88f0d13
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-05-13 23:10:12 +0530

    made num of col = 2

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import { StyleSheet, Image, FlatList } from 'react-native';
 
-import { Text, View } from '@/src/components/Themed';
 import products from '@/assets/data/products';
 import ProdctListItem from '@/src/components/ProductListItem';
 
@@ -9,6 +8,6 @@ import ProdctListItem from '@/src/components/ProductListItem';
 
 export default function TabOneScreen() {
   return (
-    <FlatList data={products} renderItem={({item}) => <ProdctListItem product={item}/> } />
+    <FlatList data={products} renderItem={({item}) => <ProdctListItem product={item}/>  } numColumns={2}/>
   );
 }
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -24,6 +24,7 @@ const styles = StyleSheet.create({
       backgroundColor: 'white',
       padding: 10,
       borderRadius: 20,
+      flex: 1
     },
     title: {
       fontSize: 18,
