975d5c7a5ea16a836dfa2fb64445a5bf0d072364
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-05-15 15:42:32 +0530

    created cart screen and cartlistitem

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,18 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useContext } from 'react'
-import { CartContext, useCart } from '../providers/CartProvider'
-import { CartItem } from '../types'
+import { View, Text, Platform, FlatList } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { useCart } from '../providers/CartProvider';
+import Button from '../components/Button';
+import CartListItem from '../components/CartListItem';
 
 
 
 const CartScreen = () => {
-  const {items} = useCart()
-  return (
-    <View>
-      <Text>Cart items length: {items.length}</Text>
-    </View>
-  )
-}
+  const { items, total } = useCart();
 
-export default CartScreen
+  return (
+    <View style={{ padding: 10 }}>
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <CartListItem cartItem={item} />}
+        contentContainerStyle={{ gap: 10 }}
+      />
+
+      <Text style={{ marginTop: 20, fontSize: 20, fontWeight: '500' }}>
+        Total: ${total}
+      </Text>
+      <Button text="Checkout" />
+
+      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+    </View>
+  );
+};
+
+export default CartScreen;
